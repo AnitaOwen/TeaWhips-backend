fd2c8ba89c501a9895ff7679ffec6d7499d47f7e
brenda-soto <bsoto@pursuit.org>
2024-03-21 14:58:24 -0400

    Schema finished

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,25 @@
-DROP DATABASE IF EXISTS myDB -- change name of db
-CREATE DATABASE myDB -- change name of db
+DROP DATABASE IF EXISTS teapots_dev
+CREATE DATABASE teapots_dev
 
-\c myDB -- change name of db
+\c teapots_dev
 
--- Create your tables here
+CREATE TABLE teapots (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  image TEXT NOT NULL,
+  price INTEGER NOT NULL,
+  description VARCHAR(200),
+  material TEXT,
+  capacity INTEGER
+);
+
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  reviewer VARCHAR(40) NOT NULL,
+  content VARCHAR(500),
+  rating NUMERIC
+  CHECK(rating >= 1 AND rating <= 5),
+  created_at TIMESTAMP,
+  teapot_id INTEGER REFERENCES teapots(id)
+  ON DELETE CASCADE
+);
